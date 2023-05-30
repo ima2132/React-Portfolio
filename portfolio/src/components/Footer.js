9bc12018e68a27c952bdcdf6185207e5fbb2881c
ima2132 <ima2132@columbia.edu>
2023-05-29 20:58:05 -0400

    worked on footer; onto rest of the components

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/Footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <a href="https://github.com/ima2132">GitHub</a>
+            <a href="https://www.linkedin.com/in/irene-akram-a384141b2/">LinkedIn</a>
+            <a href="mailto:ima2132@columbia.edu">Email</a>
+        </footer>
+    );
+}
+
