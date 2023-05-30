0cb12bd6d4e0c3001ae1afbf9987068034d47741
ima2132 <ima2132@columbia.edu>
2023-05-30 02:02:31 -0400

    worked on app.js/index.js and added styling to index.css

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedinIn, faGithub, faEnvelope, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab, faLinkedinIn, faGithub, faEnvelope, faDownload);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
