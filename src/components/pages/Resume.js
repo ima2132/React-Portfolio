ee1f0fa9e76525b0efc2825936de92357f1007e4
ima2132 <ima2132@columbia.edu>
2023-06-04 03:44:20 -0400

    styled resume; need to fix header and projects

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,40 +7,45 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 export default function Resume() {
   return (
     <section className="resume">
-      <h1></h1>
+      <h1 className="resume-heading">Resume</h1>
+      
       <div className="download">
         <h2></h2>
         <a href={ResumePDF} download><FontAwesomeIcon icon={faDownload} /> Download Resume</a>
       </div>
+
       <div>
         <h3 className="front">Front-End Proficiencies</h3>
         <ul className="list-group">
           <li className="list-group-item d-flex justify-content-between align-items-center">
             CSS
-            <span className="badge badge-primary badge-pill">14</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-center">
             JavaScript
-            <span className="badge badge-primary badge-pill">2</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-center">
             React
-            <span className="badge badge-primary badge-pill">2</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
         </ul>
+      </div>
+      
+      <div>
         <h3 className="back">Back-End Proficiencies</h3>
         <ul className="list-group">
           <li className="list-group-item d-flex justify-content-between align-items-center">
             Node.js
-            <span className="badge badge-primary badge-pill">14</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-center">
             Express.js
-            <span className="badge badge-primary badge-pill">2</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
           <li className="list-group-item d-flex justify-content-between align-items-center">
             Jest
-            <span className="badge badge-primary badge-pill">1</span>
+            <span className="badge badge-primary badge-pill"></span>
           </li>
         </ul>
       </div>
