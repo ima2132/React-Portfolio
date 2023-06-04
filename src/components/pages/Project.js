f476704025d040388df233cf84dc258fd49be86f
ima2132 <ima2132@columbia.edu>
2023-06-04 04:01:14 -0400

    styled project.css; need to fix header/footer

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -5,8 +5,8 @@ import '../styles/Project.css';
 const Project = ({ projects }) => {
 
     const renderProject = (project) => (
-        <div className="card p-grid-item" key={project.id}>
-            <img className="p-image" src={project.img} alt={project.title}></img>  
+        <div className="card-grid" key={project.id}>
+            <img className="p-img" src={project.img} alt={project.title}></img>  
             <a href={project.deployed} className="card-link">{project.title}</a>
             <a href={project.github} className="card-link"><FontAwesomeIcon icon="Fa-Github" /></a>
         </div> 
