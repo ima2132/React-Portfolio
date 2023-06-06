92576a477e3f7fc7f84abcdb7c5bc71c70dc4e51
ima2132 <ima2132@columbia.edu>
2023-06-06 16:10:27 -0400

    styled about me; contact; and nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,56 +3,39 @@ import React from 'react';
 function NavTabs({ currentPage, handlePageChange }) {
   const navLinkStyle = {
     color: '#000000', // set the font color to black
-    backgroundColor: 'transparent', // set the background color for tabs as transparent
-    border: 'none', // removed the border
+    fontFamily: 'Press Start 2P', // set the font family
+    fontSize: '15px', // set the font size
+    backgroundColor: '#c3c3c3', // set background color to #c3c3c3 for all tabs
+    border: '6px solid #c3c3c3', // set a solid border with the same color as the background
+    borderRadius: '0px', // remove the border radius
   };
-
+  
   return (
     <nav className="navbar sticky-top" style={{ backgroundColor: 'transparent', height: '80px', width: '100%', lineHeight: '40px', position: 'relative', borderBottom: 'none' }}>
       <h3 className="navbar-name" style={{ position: 'absolute', top: '20px', left: '11px', fontWeight: 'bold', fontSize: '35px' }}>IRENE AKRAM</h3>
 
-      <ul className="nav nav-tabs" style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-        <li className="nav-item">
-          <a
-            href="#about"
-            onClick={() => handlePageChange('About')}
-            className={`nav-link ${currentPage === 'About' ? 'active' : ''}`}
-            style={navLinkStyle}
-          >
+      <menu role="tablist" style={{ outline: 'none', display: 'flex', gap: '10px', position: 'absolute', top: '4px', left: '469px' }}>
+        <li role="tab" aria-selected={currentPage === 'About'}>
+          <a href="#about" onClick={() => handlePageChange('About')} className={`nav-link ${currentPage === 'About' ? 'active' : ''}`} style={navLinkStyle}>
             About
           </a>
         </li>
-        <li className="nav-item">
-          <a
-            href="#portfolio"
-            onClick={() => handlePageChange('Portfolio')}
-            className={`nav-link ${currentPage === 'Portfolio' ? 'active' : ''}`}
-            style={navLinkStyle}
-          >
+        <li role="tab" aria-selected={currentPage === 'Portfolio'}>
+          <a href="#portfolio" onClick={() => handlePageChange('Portfolio')} className={`nav-link ${currentPage === 'Portfolio' ? 'active' : ''}`} style={navLinkStyle}>
             Portfolio
           </a>
         </li>
-        <li className="nav-item">
-          <a
-            href="#contact"
-            onClick={() => handlePageChange('Contact')}
-            className={`nav-link ${currentPage === 'Contact' ? 'active' : ''}`}
-            style={navLinkStyle}
-          >
+        <li role="tab" aria-selected={currentPage === 'Contact'}>
+          <a href="#contact" onClick={() => handlePageChange('Contact')} className={`nav-link ${currentPage === 'Contact' ? 'active' : ''}`} style={navLinkStyle}>
             Contact
           </a>
         </li>
-        <li className="nav-item">
-          <a
-            href="#resume"
-            onClick={() => handlePageChange('Resume')}
-            className={`nav-link ${currentPage === 'Resume' ? 'active' : ''}`}
-            style={navLinkStyle}
-          >
+        <li role="tab" aria-selected={currentPage === 'Resume'}>
+          <a href="#resume" onClick={() => handlePageChange('Resume')} className={`nav-link ${currentPage === 'Resume' ? 'active' : ''}`} style={navLinkStyle}>
             Resume
           </a>
         </li>
-      </ul>
+      </menu>
     </nav>
   );
 }
@@ -61,4 +44,8 @@ export default NavTabs;
 
 
 
+
+
+
+    
 
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,21 +5,34 @@ import '../styles/About.css';
 export default function About() {
   return (
     <section className="my-5">
-      <h1 id="about">About Me</h1>
-      <img src={me} alt="" className="me" style={{ display: 'block', margin: '20px auto' }} />
+      <img src={me} alt="" className="me" />
 
-      <p className="intro">
-        Welcome to my vibrant portfolio! I'm Irene, a passionate individual who recently earned a Master's degree from Columbia University. After completing my graduate studies, my goal was to explore opportunities in the tech industry that would allow me to merge my creative pursuits and technology into a single integrated role. 
-      </p>
-
-      <p className="intro">
-        Within this portfolio, you'll discover a collection of dynamic applications that I've personally crafted and deployed. 
-The projects I showcase here highlight my dedication to creativity, proficiency in using advanced technologies, and a keen eye for aesthetics. Feel free to explore the results of my ongoing pursuit of self-improvement.      </p>
-
-      <p className="intro">
-        Thank you for taking the time to visit my portfolio, and I look forward to connecting with you soon!
-      </p>
+      <div className="window" style={{ width: '550px' }}>
+        <div className="title-bar">
+          <div className="title-bar-text">About Me</div>
+          <div className="title-bar-controls">
+            <button aria-label="Minimize" className="button"></button>
+            <button aria-label="Maximize" className="button"></button>
+            <button aria-label="Close" className="button"></button>
+          </div>
+        </div>
+        <div className="window-body">
+        <p className="intro">
+         Hello, welcome to my vibrant portfolio!
+        </p>
+         <p className="intro">
+            I'm Irene, a passionate individual who recently earned a Master's degree from Columbia University. After completing my graduate studies, my goal was to explore opportunities in the tech industry that would allow me to merge my creative pursuits and technology into a single integrated role.
+          </p>
+          <p className="intro">
+            Within this portfolio, you'll discover a collection of dynamic applications that I've personally crafted and deployed. The projects I showcase here highlight my dedication to creativity, proficiency in using advanced technologies, and a keen eye for aesthetics. Feel free to explore the results of my ongoing pursuit of self-improvement.
+          </p>
+          <p className="intro">
+            Thank you for taking the time to visit my portfolio, and I look forward to connecting with you soon!
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
+
 
